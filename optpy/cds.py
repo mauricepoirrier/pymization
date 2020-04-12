4f895b4f9869da970be32dd533700724e9c4e946
mauricepoirrier <mauricepoirrier@gmail.com>
2020-04-12 14:17:59 -0400

    feat: CDS implementation

--- optpy/cds.py
+++ optpy/cds.py
@@ -1,4 +1,6 @@
 from algorithms import Algorithms
+from numpy import Infinity, sum
+import logging
 
 
 class CDS(Algorithms):
@@ -9,3 +11,59 @@ class CDS(Algorithms):
         self.n_nodes = 0
         if nodes is not None:
             self.n_nodes = len(self.nodes.shape[0])
+            self.n_jobs = len(self.nodes.shape[1])
+        self.final_sequence = []
+        self.objetive_function = Infinity
+        self.first_mock_machine = []
+        self.second_mock_machine = []
+        self.first_nodes_set = []
+        self.second_nodes_set = []
+
+    def run(self):
+        self.objetive_function = Infinity
+        for i in range(self.n_nodes - 1):  # permutation numbers
+            self.first_mock_machine, self.second_mock_machine = [], []
+            self.first_node_set, self.second_node_set = [], []
+            self._get_sets(i)
+            self.first_node_set.sort()
+            self.second_mock_machine.sort(reverse=True)
+            self._previos_job(list(self.first_node_set + self.second_mock_machine))
+        logging.info(
+            f"Best seq: {self.final_sequence} | Objective function: {self.objetive_function}"
+        )
+
+    def _get_sets(self, permutation):
+        for j in range(self.nodes.size):
+            self.first_mock_machine.append(sum(self.nodes[j][: (permutation + 1)]))
+            self.second_mock_machine.append(
+                sum(self.nodes[j]) - sum(self.nodes[j][: (permutation + 1)])
+            )
+            if self.first_mock_machine[j] < self.second_mock_machine[j]:
+                self.first_node_set.append(j)
+            else:
+                self.second_node_set.append(j)
+
+    def _update_objective_function(self, time_job, seq):
+        if self.objetive_function > time_job[-1]:
+            self.objetive_function = time_job[-1]
+            self.final_sequence = seq.copy()
+
+    def _previos_job(self, node_sequence):
+        time_previous_job = [0] * self.n_nodes
+        time_previous_job[0] = self.nodes[node_sequence[0]][0]
+        for i in range(1, self.n_nodes):
+            time_previous_job[i] = (
+                self.nodes[node_sequence[0]][i] + time_previous_job[i - 1]
+            )
+        time_job = [0] * self.n_nodes
+        iterSecuencia = iter(node_sequence)
+        next(iterSecuencia)
+        for _, i in enumerate(iterSecuencia):
+            time_job[0] = time_previous_job[0] + self.nodes[i][0]
+            for j in range(1, self.n_nodes):
+                if time_previous_job[j] >= time_job[j - 1]:
+                    time_job[j] = self.nodes[i][j] + time_previous_job[j]
+                else:
+                    time_job[j] = self.nodes[i][j] + time_job[j - 1]
+            time_previous_job = time_job.copy()
+        self._update_objective_function(time_job, node_sequence)
--- optpy/solver.py
+++ optpy/solver.py
@@ -1,4 +1,5 @@
 import numpy as np
+from cds import CDS
 
 
 class Solver:
@@ -8,6 +9,23 @@ class Solver:
     def __init__(self, model=None):
         self.model_name = model
         self.nodes = None
+        self.instance = None
 
     def load_data_csv(self, path_file, skip_rows=0):
         np.genfromtxt(path_file, delimiter=",", skiprows=skip_rows)
+
+    def run(self, model):
+        if model is not None:
+            self.model_name is None
+        if self.model_name is None:
+            raise RuntimeError("There wasn't any model defined")
+        self._instance_by_name()
+        self.instance.run()
+
+    def _instance_by_name(self):
+        if self.nodes is None:
+            raise RuntimeError("First load the data using load_data_csv")
+        instance_helper = {"CDS": CDS(self.nodes)}
+        if self.model_name not in instance_helper:
+            raise RuntimeError("Algorithm is not yet implemented")
+        self.instance = instance_helper[self.model_name]
